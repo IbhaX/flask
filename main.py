ed0b43db95cdcb4abc786aa499b8db5bdf206418
Albin Anthony (Ibha-X) <89918383+IbhaX@users.noreply.github.com>
2023-02-27 10:29:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+@app.route('/callback', methods=['GET', 'POST'])
+def callback():
+    code = request.url.split('code=')[-1]
+    if request.args.get('format') == 'json':
+        return jsonify({"code": code})
+    else:
+        return f"""<h1> Authorized code </h1>
+                    <h3>{code}</h3>
+                """
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(host="0.0.0.0", port=8888)
