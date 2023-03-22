c57303b379796a407ab6222dacd3244b9f898c42
Albin Anthony (Ibha-X) <89918383+IbhaX@users.noreply.github.com>
2023-03-22 08:04:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/callback', methods=['GET', 'POST'])
+@app.get("/")
+def home():
+    return "Custom Redirect URI"
+
+@app.route("/callback", methods=['GET', 'POST'])
 def callback():
     code = request.url.split('code=')[-1]
-    if request.args.get('format') == 'json':
+    if request.args.get('format') == "json":
         return jsonify({"code": code})
-    elif request.args.get('format') == 'plain':
+    elif request.args.get('format') == "plain":
         return f"<h1>{code}</h1>"
-    else:
+    elif request.args.get('format') == "html":
         return f"""<h1> Authorized code </h1>
                     <h3>{code}</h3>
                 """
+    else:
+        return "Got an empty request..."
+                
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8888)
